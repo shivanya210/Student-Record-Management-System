fb58614fceee68e4065ba05fbaf189e2fede15db
shivanya210 <32261618+shivanya210@users.noreply.github.com>
2017-11-09 11:10:22 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
-$database_host ="localhost";
-$database_user ="root";
-$database_password ="shivani@123";
-$database_name ="student";
-$conn=new mysqli($database_host,$database_user,$database_password,$database_name)
-if($conn->connect_error)
+$servername = "localhost";
+$username = "root";
+$password = "shivani@123";
+$dbname = "student";
+$conn = new mysqli($servername,$username,$password,$dbname);
+if ($conn->connect_error)
 {
-die("failed" .mysqli_connect_error());
-  
+	die("failed  " .mysqli_connect_error());
+
 }
-else
-echo"connected";
+
+//echo "connected to db \n";
+
 ?>
