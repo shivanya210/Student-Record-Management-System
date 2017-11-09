5646d7a98ac3f7564eb863311f9abbee4893c67c
shivanya210 <32261618+shivanya210@users.noreply.github.com>
2017-11-09 11:11:53 +0530

    Create index.php

---
+++ SMS/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+
+	<head>
+		<title>SMS</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="main.css" />
+                
+		
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			
+				<header id="header" class="alt">
+					<h1><a href="index.html">Student Management System</a> by Shivani Singh</h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							
+							        </ul>		
+							
+						</ul>
+					</nav>
+				</header>
+
+			
+				<section id="banner">
+					<h2>Student Management System</h2>
+					<p>Select your query</p>
+					<ul class="actions">
+						<li><a href="view.php" class="button">VIEW</a></li>
+						<li><a href="insert.php" class="button">INSERT</a></li>
+						<li><a href="delete.php" class="button">DELETE</a></li>
+					</ul>
+				</section>
+
+				<
+
+			
+			
+
+		</div>
+
+	
+			
+
+	</body>
+</html>
